63f681c96b37b05ca2dbdf8cdf92efc47ad988d0
Dornam0930 <warhammer925@yahoo.com>
2024-06-19 12:19:08 -0700

    getURLsFromHTML() and associated tests implemented

--- crawl.js
+++ crawl.js
@@ -1,6 +1,30 @@
+import { JSDOM } from 'jsdom'
+
 function normalizeURL(url) {
     const urlObj = new URL(url)
     return `${urlObj.hostname}/${urlObj.pathname.replaceAll('/','')}`
 }
 
-export { normalizeURL };
+function getURLsFromHTML(htmlbody, html) {
+    const links = []
+    const hmtljsdom = new JSDOM(htmlbody)
+    const anchors = hmtljsdom.window.document.querySelectorAll('a')
+
+    for (const anchor of anchors) {
+        if (anchor.hasAttribute('href')) {
+            let href = anchor.getAttribute('href')
+            console.log(`anchor href: ${href}`)
+            try {
+                href = new URL(href, html).href
+                console.log(`URL.href: ${href}`)
+                links.push(href)
+            } catch (error) {
+                console.log(`${error.message}: ${href.hostname}`)
+            }
+        }
+    }
+
+     return links
+}
+
+export { normalizeURL, getURLsFromHTML };
--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,11 @@
 import { jest } from "@jest/globals";
 import { test, expect } from "@jest/globals";
-import { normalizeURL } from "./crawl.js";
+import { normalizeURL, getURLsFromHTML } from "./crawl.js";
+
+const rootURL = 'https://blog.boot.dev'
+const testBody = '<html><body><a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a></body></html>'
+const testBody2 = '<html><body><a href="/one"><span>Go to Boot.dev/one</span></a><a href="/two"><span>Go to Boot.dev/two</span></a></body></html>'
+const testBody3 = '<html><body><a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a><a href="/one"><span>Go to Boot.dev/one</span></a></body></html>'
 
 test('normalizes https://blog.boot.dev/path/ to blog.boot.dev/path', () => {
     expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
@@ -16,4 +21,21 @@ test('normalizes http://blog.boot.dev/path/ to blog.boot.dev/path', () => {
 
 test('normalizes http://blog.boot.dev/path to blog.boot.dev/path', () => {
     expect(normalizeURL('http://BLOG.boot.dev/path')).toBe('blog.boot.dev/path');
+})
+
+test('getURLsFromHTML absolute', () => {
+    const testURL = rootURL + '/'
+    expect(getURLsFromHTML(testBody, rootURL)).toEqual([testURL]);
+})
+
+test('getURLsFromHTML relative', () => {
+    const testURL = rootURL + '/one'
+    const testURL2 = rootURL + '/two'
+    expect(getURLsFromHTML(testBody2, rootURL)).toEqual([testURL, testURL2]);
+})
+
+test('getURLsFromHTML mixed', () => {
+    const testURL = rootURL + '/'
+    const testURL2 = rootURL + '/one'
+    expect(getURLsFromHTML(testBody3, rootURL)).toEqual([testURL, testURL2]);
 })
